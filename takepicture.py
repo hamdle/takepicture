63dfbbe24cc194a11c76be154485a26627ad0636
Eric Jawaid Marty <ericmarty@protonmail.com>
2024-07-20 17:54:15 -0500

    Clean and refactor python script

--- takepicture.py
+++ takepicture.py
@@ -1,60 +1,78 @@
-import pygame
 import pygame.camera
 from datetime import datetime
-import time
 import requests
 import cv2
 import base64
 
 version = 2
-take_in_sec = 60 * 3
 path = "/home/eric/Pictures/TakePicture/"
 key = "SANGEAN6666!"
-url = "https://workout.dev/api/takepicture"
+url = "http://workout.dev.ddev.site/api"
+id = "B"
 
 def filename():
-	return path+"TakePicture v"+str(version)+" - {:%B %d, %Y %I:%M:%S %p-B}".format(datetime.now())+".jpg"
+	return "TakePicture v" + str(version) + " - {:%B %d, %Y %I:%M:%S %p}".format(datetime.now()) + "-" + id + ".jpg"
 
-print("Booting camera...")
+def log(msg):
+    print("{:%B %d, %Y %I:%M:%S %p}".format(datetime.now()) + "-" + id + " :: " + msg)
+
+log("Initializing cameras...")
 pygame.camera.init()
 cameras = pygame.camera.list_cameras()
-print(cameras)
-print(cameras[1])
 
-print("Directory: " + path)
-print("Interation (sec): " + str(take_in_sec))
-print("Creating camera...")
 if cameras:
-	camera = pygame.camera.Camera(cameras[0], (1920, 1080))
+    log("Loading camera...")
+    camera = pygame.camera.Camera(cameras[0], (1920, 1080))
+    if camera is None:
+        log("Failed to load camera...")
+        log("Exiting program.")
+        exit()
+    log(cameras[0])
 else:
-	print("No cameras found.")
-	exit()
+    log("No cameras found...")
+    log("Exiting program.")
+    exit()
 
-print("Taking pictures...")
+name_img = filename()
+path_img = path + name_img
 
+log("Taking picture...")
 
-path_img = filename()
-name_img = "TakePicture v"+str(version)+" - {:%B %d, %Y %I:%M:%S %p-B}".format(datetime.now())+".jpg"
-print(name_img)
+camera.start()
+image = camera.get_image()
+log("Saving picture...")
+pygame.image.save(image, path_img)
+camera.stop()
+log(path_img)
 
-if cameras:
-    camera.start()
-    image = camera.get_image()
-    pygame.image.save(image, path_img)
-    camera.stop()
+img = cv2.imread(path_img)
+b64_img = base64.b64encode(cv2.imencode('.jpg', img)[1])    # .decode()
 
-    img = cv2.imread(path_img)
-    #string_img = base64.b64encode(cv2.imencode('.jpg', img)[1]).decode()
-    string_img = base64.b64encode(cv2.imencode('.jpg', img)[1])
-    print("Sending " + "https://workout.dev/api/takepicture" + "..")
-    data = {
-        "key": key,
-        "name": name_img,
-        "image": string_img
-    }
-    headers={"Content-Type": "application/json"}
-    response = requests.post("https://workout.dev/api/takepicture",
-        data,
-        headers,
-    )
-    print(response.json())
+data = {
+    "key": key,
+    "name": name_img,
+    "image": b64_img,
+    "method": "Picture.takepicture",
+}
+headers={"Content-Type": "application/json"}
+
+try:
+    log("Sending picture...")
+    log(url)
+    response = requests.post(url,data,headers)
+except Exception as ex:
+    log("Error sending picture...")
+    log(ex.message)
+    log("Exiting program.")
+    exit()
+
+try:
+    log("Reading response...")
+    log(str(response.json()))
+except requests.JSONDecodeError:
+    log("Error decoding response...")
+    log(response.text)
+    log("Exiting.")
+    exit()
+
+log("End.")
