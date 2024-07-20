ddf3e4cb0bfc27257173b66c548162bf46590550
Eric Jawaid Marty <ericmarty@protonmail.com>
2024-07-20 13:46:02 -0500

    Clean up run.php

--- run.php
+++ run.php
@@ -1,15 +1,23 @@
 <?php
 
+// Usage:
+// php run.php /home/eric/repos/takepicture 3
+
 $file = 'takepicture.py';
-$path = getcwd();
+$path = $argc > 1
+    ? $argv[1]  // /home/eric/repos/takepicture
+    : getcwd();
 $cmd = 'python '.$path.'/'.$file;
+$mins = $argc > 2
+    ? $argv[2] // 3
+    : 10;
 
 while (true)
 {
     echo 'Executing command: '.$cmd.PHP_EOL;
     $tmp = shell_exec($cmd);
     echo $tmp;
-    sleep(60 * 10);
+    sleep(60 * $mins);
 }
 
 ?>
